2909b72abcfa31861922191d8be24f57267b2ed4
Rares <>
2025-02-27 08:00:43 +0200

    Add dynamic loading of assembly

---
+++ 01-others/LoadAndExecute.Library/Dog.cs
@@ -0,0 +1,9 @@
+﻿namespace LoadAndExecute.Library;
+
+public class Dog
+{
+  public void Speak(string? name)
+  {
+    WriteLine($"Woof! My name is {name}");
+  }
+}
---
+++ 01-others/ReflectionInAction/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection; // Assembly
+Assembly? thisAssembly = Assembly.GetEntryAssembly();
+
+if (thisAssembly is null)
+{
+  WriteLine("Could not get the entry assembly.");
+  return;
+}
+
+OutputAssemblyInfo(thisAssembly);
+
+WriteLine("Creating load context for:\n  {0}\n", 
+  Path.GetFileName(thisAssembly.Location));
+
+DemoAssemblyLoadContext loadContext = new(thisAssembly.Location);
+
+string assemblyPath = Path.Combine(
+  Path.GetDirectoryName(thisAssembly.Location) ?? "", 
+  "LoadAndExecute.Library.dll");
+
+WriteLine("Loading:\n  {0}\n",
+  Path.GetFileName(assemblyPath));
+
+Assembly dogAssembly = loadContext.LoadFromAssemblyPath(assemblyPath);
+
+OutputAssemblyInfo(dogAssembly);
+
+Type? dogType = dogAssembly.GetType("LoadAndExecute.Library.Dog");
+
+if (dogType is null)
+{
+  WriteLine("Could not get the Dog type.");
+  return;
+}
+
+MethodInfo? method = dogType.GetMethod("Speak");
+
+if (method != null)
+{
+  object? dog = Activator.CreateInstance(dogType);
+
+  for (int i = 0; i < 10; i++)
+  {
+    method.Invoke(dog, new object[] { "Fido" });
+  }
+}
+
+WriteLine();
+WriteLine("Unloading context and assemblies.");
+loadContext.Unload();
