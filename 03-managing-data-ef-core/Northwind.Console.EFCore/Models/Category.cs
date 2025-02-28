bc744143a75fdca5d2a3675f2bdbab24ee974175
Rares <>
2025-02-28 07:03:38 +0200

    Useing efcore cli to scaffold models, use envs to store db connection

---
+++ 03-managing-data-ef-core/Northwind.Console.EFCore/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.Models;
+
+[Index("CategoryName", Name = "CategoryName")]
+public partial class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+
+    [StringLength(15)]
+    public string CategoryName { get; set; } = null!;
+
+    [Column(TypeName = "ntext")]
+    public string? Description { get; set; }
+
+    [Column(TypeName = "image")]
+    public byte[]? Picture { get; set; }
+    // So this is saying: "The Products 
+    // collection in the Category class is 
+    // related to the Category property in the Product class."
+    [InverseProperty("Category")]
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # turbo bassoon
+
+_To scaffold models from an existing db_
+
+```bash
+dotnet ef dbcontext scaffold "Data Source=tcp:127.0.0.1,1433;Initial Catalog=Northwind;Integrated Security=true;TrustServerCertificate=true;" Microsoft.EntityFrameworkCore.SqlServer --output-dir Models --namespace Northwind.Models --data-annotations --context NorthwindDb
+```
