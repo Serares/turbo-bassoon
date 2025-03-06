c8f0b4da68fad4573f9a71705934b8165ea4bad7
Rares <>
2025-03-06 08:11:19 +0200

    Add console app to use mappings and a test to assert mappings

---
+++ 06-libraries/MappingObjects.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MappingObjects.Mappers;
+using Northwind.EntityModels;
+using Northwind.ViewModels;
+using System.Text;
+
+OutputEncoding = Encoding.UTF8; // to suport unicode characters Euro currency symbol
+
+Cart cart = new(
+    Customer: new(
+        FirstName: "John",
+        LastName: "Doe"
+    ),
+    Items: new() {
+        new(ProductName: "Apples", UnitPrice: 0.49M, Quantity: 10),
+        new(ProductName: "Bananas", UnitPrice: 0.99M, Quantity: 4)
+    }
+);
+
+WriteLine("*** Original data before mapping.");
+WriteLine($"{cart.Customer}");
+foreach (LineItem item in cart.Items)
+{
+    WriteLine($"{item}");
+}
+
+MapperConfiguration config = CartToSummaryMapper.GetMapperConfiguration();
+
+IMapper mapper = config.CreateMapper();
+
+Summary summary = mapper.Map<Cart, Summary>(cart);
+
+WriteLine("");
+WriteLine("*** After mapping");
+WriteLine($"Summary: {summary.FullName} spend {summary.Total}");
+
