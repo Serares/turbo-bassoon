fc6df7fdb955e2a6d0566e5c5734ac9001306155
Rares <>
2025-03-06 08:01:14 +0200

    Add tests for model mapping configuration

---
+++ 06-libraries/MappingObjects.Tests/TestAutomapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper; // To use MapperConfiguration
+using MappingObjects.Mappers;
+namespace MappingObjects.Tests;
+
+public class TestAutoMapperConfig
+{
+
+    [Fact]
+    public void TestSummaryMapping()
+    {
+        MapperConfiguration config = CartToSummaryMapper.GetMapperConfiguration();
+
+        config.AssertConfigurationIsValid();
+    }
+}
