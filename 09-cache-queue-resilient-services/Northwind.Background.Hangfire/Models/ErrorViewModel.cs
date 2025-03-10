9eb9cd870eee06ed3632e299365b3e4f85748362
Rares <>
2025-03-10 10:56:05 +0200

    Not needed project files

--- 09-cache-queue-resilient-services/Northwind.Background.Hangfire/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Northwind.Background.Hangfire.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
