3c410ab5083947303e686371e7ff20e0767c2301
Rares <>
2025-03-10 08:12:29 +0200

    Add client and randomize error in service

---
+++ 09-cache-queue-resilient-services/Northwind.WebApi.Client.Mvc/Program.cs
@@ -0,0 +1,39 @@
+using System.Net.Http.Headers; // to use metiatypequalityhedaer
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient(name: "Northwind.WebApi.Service",
+configureClient: options =>
+{
+    options.BaseAddress = new("https://localhost:5091/");
+    options.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue(
+            "application/json", 1.0
+        )
+    );
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
