03da2601f9112ebea9bf7088ffeaf15307ff22ef
Rares <>
2025-03-11 12:06:43 +0200

    Add data models

---
+++ 11-signalr/Northwind.Common/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Northwind.Chat.Models;
+
+public class UserModel
+{
+    public string Name { get; set; } = null!;
+    public string ConnectionId { get; set; } = null!;
+    public string? Groups { get; set; } // comma-separated list ❗in real world would use string[]
+}
